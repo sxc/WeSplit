1df07eaddfa59c88c0de9c2597bc7b30b8227304
sxc <qiuyequ@icloud.com>
2019-10-21 15:27:10 +0800

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Xiaochun Shen on 2019/10/21.
+//  Copyright © 2019 Shen Xiaochun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
